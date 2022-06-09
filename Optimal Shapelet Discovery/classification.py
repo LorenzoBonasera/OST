0e5bff5c8732e8375bcbbdf304723c9f684a5366
LorenzoBonasera <95094358+LorenzoBonasera@users.noreply.github.com>
2022-06-09 12:54:44 +0200

    Add files via upload

--- Optimal Shapelet Discovery/classification.py
+++ Optimal Shapelet Discovery/classification.py
@@ -5,32 +5,62 @@ import sktime.datatypes._panel._convert as conv
 import matplotlib.pyplot as plt
 
 
-def predictModel(A, A_hat, b, d, labels, X_test, y_test, exemplar):
+def predictModel(A, A_hat, b, labels, X_test, y_test, exemplar):
     t_rows, t_cols = X_test.shape
     tmp = np.zeros([t_rows, 1], dtype=int)
     Y_predict = np.hstack((np.reshape(y_test, (t_rows, 1)), tmp))
-    num_nodes = len(d)
+    num_labels = len(set(labels))
 
     # split
-    if num_nodes > 1:
+    if 5 <= num_labels <= 8:
+        for i in range(t_rows):
+            if np.linalg.norm(np.dot(A_hat[0], exemplar) - np.dot(A[0], X_test[i, :]), ord=1) < b[0]:
+                if np.linalg.norm(np.dot(A_hat[1], exemplar) - np.dot(A[1], X_test[i, :]), ord=1) < b[1]:
+                    if np.linalg.norm(np.dot(A_hat[3], exemplar) - np.dot(A[3], X_test[i, :]), ord=1) < b[3]:
+                        Y_predict[i, 1] = labels[0]
+
+                    else:
+                        Y_predict[i, 1] = labels[1]
+
+                elif np.linalg.norm(np.dot(A_hat[4], exemplar) - np.dot(A[4], X_test[i, :]), ord=1) < b[4]:
+                    Y_predict[i, 1] = labels[2]
+
+                else:
+                    Y_predict[i, 1] = labels[3]
+
+            elif np.linalg.norm(np.dot(A_hat[2], exemplar) - np.dot(A[2], X_test[i, :]), ord=1) < b[2]:
+                if np.linalg.norm(np.dot(A_hat[5], exemplar) - np.dot(A[5], X_test[i, :]), ord=1) < b[5]:
+                    Y_predict[i, 1] = labels[4]
+
+                else:
+                    Y_predict[i, 1] = labels[5]
+
+            elif np.linalg.norm(np.dot(A_hat[6], exemplar) - np.dot(A[6], X_test[i, :]), ord=1) < b[6]:
+                Y_predict[i, 1] = labels[6]
+
+            else:
+                Y_predict[i, 1] = labels[7]
+
+    elif 3 <= num_labels <= 4:
         for i in range(t_rows):
             if np.linalg.norm(np.dot(A_hat[0], exemplar) - np.dot(A[0], X_test[i, :]), ord=1) < b[0]:
                 if np.linalg.norm(np.dot(A_hat[1], exemplar) - np.dot(A[1], X_test[i, :]), ord=1) < b[1]:
                     Y_predict[i, 1] = labels[0]
 
-                elif np.linalg.norm(np.dot(A_hat[1], exemplar) - np.dot(A[1], X_test[i, :]), ord=1) >= b[1]:
+                else:
                     Y_predict[i, 1] = labels[1]
 
-            elif np.linalg.norm(np.dot(A_hat[0], exemplar) - np.dot(A[0], X_test[i, :]), ord=1) >= b[0]:
-                if np.linalg.norm(np.dot(A_hat[2], exemplar) - np.dot(A[2], X_test[i, :]), ord=1) < b[2]:
-                    Y_predict[i, 1] = labels[2]
+            elif np.linalg.norm(np.dot(A_hat[2], exemplar) - np.dot(A[2], X_test[i, :]), ord=1) < b[2]:
+                Y_predict[i, 1] = labels[2]
 
-                elif np.linalg.norm(np.dot(A_hat[2], exemplar) - np.dot(A[2], X_test[i, :]), ord=1) >= b[2]:
-                    Y_predict[i, 1] = labels[3]
+            else:
+                Y_predict[i, 1] = labels[3]
+
     else:
         for i in range(t_rows):
             if np.linalg.norm(np.dot(A_hat[0], exemplar) - np.dot(A[0], X_test[i, :]), ord=1) < b[0]:
                 Y_predict[i, 1] = labels[0]
+
             else:
                 Y_predict[i, 1] = labels[1]
 
